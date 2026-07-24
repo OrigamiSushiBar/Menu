4b95879614208bdb681701780f6735e67bfb80a4
OrigamiSushiBar <rayanbouthour9@gmail.com>
2026-07-24 19:43:04 +0200

    Change image format from .webp to .jpg

--- script.js
+++ script.js
@@ -313,7 +313,7 @@ const products = [
         descriptionFR: "Bouillon traditionnel Japonais à base de pate miso",
         descriptionEN: "Traditional Japanese broth made from miso paste",
         price: "8,000 DT",
-        image: "images/8.webp",
+        image: "images/8.jpg",
         category: "entrees",
         subcategory: "soupes",
     },
